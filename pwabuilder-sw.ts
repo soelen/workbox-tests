5500608cf4d85c9fa2d94a2e9fc24726ed42d7d0
Uwe Sölen Flaig <uwe.soelen.flaig@gmail.com>
2021-08-22 22:32:42 +0200

    Trying some strats and routes.

--- pwabuilder-sw.ts
+++ pwabuilder-sw.ts
@@ -1,6 +1,8 @@
 import {precacheAndRoute, createHandlerBoundToURL} from 'workbox-precaching'
 import {NavigationRoute, registerRoute} from 'workbox-routing'
-import { } from 'workbox-routing';
+import {CacheFirst, StaleWhileRevalidate} from 'workbox-strategies';
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
+import {ExpirationPlugin} from 'workbox-expiration';
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -40,3 +42,41 @@ catch (err) {
 //     })
 //   );
 // });
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+registerRoute(
+  ({url}) => url.origin === 'https://fonts.googleapis.com',
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+registerRoute(
+  ({url}) => url.origin === 'https://fonts.gstatic.com',
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({request}) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
