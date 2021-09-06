bfd177403ae72a4fa68a3877e5f41d623943e6ca
Uwe Sölen Flaig <uwe.soelen.flaig@gmail.com>
2021-09-06 12:46:58 +0200

    Cache clear test.

--- src/script/pages/app-index.ts
+++ src/script/pages/app-index.ts
@@ -42,7 +42,7 @@ if ('serviceWorker' in navigator) {
     // `event.isUpdate` will be true if another version of the service
     // worker was controlling the page when this version was registered.
     if (!event.isUpdate) {
-      console.log('Service worker activated for the first time!');
+      console.dir('Service worker activated for the first time!');
 
       // If your service worker is configured to precache assets, those
       // assets should all be available now.
@@ -51,12 +51,12 @@ if ('serviceWorker' in navigator) {
 
   wb.addEventListener('installed', (event) => {
     if (!event.isUpdate) {
-      console.log( 'First-installed code goes here...');
+      console.dir( 'First-installed code goes here...');
     }
   });
 
   wb.addEventListener('waiting', (event) => {
-    console.log(`A new service worker has installed, but it can't activate` +
+    console.dir(`A new service worker has installed, but it can't activate` +
       `until all tabs running the current version have fully unloaded.`);
   });
 
@@ -64,7 +64,7 @@ if ('serviceWorker' in navigator) {
     if (event.data.type === 'CACHE_UPDATED') {
       const {updatedURL} = event.data.payload;
 
-      console.log(`A newer version of ${updatedURL} is available!`);
+      console.dir(`A newer version of ${updatedURL} is available!`);
     }
   });
 
@@ -120,7 +120,7 @@ export class AppIndex extends LitElement {
       //   if (event.isUpdate) {
       //     this._update = true;
       //   } else {
-      //     console.log( 'First-installed code goes here...' );
+      //     console.dir( 'First-installed code goes here...' );
       //   }
       // } );
 
